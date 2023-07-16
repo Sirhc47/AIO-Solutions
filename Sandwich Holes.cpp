cd87414055a02c3390bdc27048baf8e0ab5da843
Sirhc47 <125524491+Sirhc47@users.noreply.github.com>
2023-07-16 14:25:49 +1000

    Create Sandwich Holes.cpp

---
+++ Sandwich Holes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <string>
+#define speed()                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(0)
+#define file(name)                       \
+    speed();                             \
+    freopen(#name "in.txt", "r", stdin); \
+    freopen(#name "out.txt", "w", stdout)
+#define dbg(x) cout << '>' << #x << ':' << x << endl;
+using namespace std;
+int main()
+{
+    file(holes);
+    int l, w;
+    int a, b;
+    cin >> l >> w >> a >> b;
+    cout << (l/a) * (w/b); 
+}
