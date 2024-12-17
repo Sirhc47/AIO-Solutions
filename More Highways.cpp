128f9d68f6f5b967638ee31e7bd0a3fa59a04081
Chris Qiu <125524491+Sirhc47@users.noreply.github.com>
2024-12-17 18:12:41 +1100

    Create More Highways.cpp

---
+++ More Highways.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    // Put your code here
+    freopen("morehigh.in", "r", stdin);
+    freopen("morehigh.out", "w", stdout);
+    cout << ("0 1 4 4 4 5 5\n1 0 3 5 3 4 4\n4 3 0 8 6 7 1\n4 5 8 0 3 4 9\n4 3 6 3 0 1 7\n5 4 7 4 1 0 7\n5 4 1 9 7 7 0");
+}
